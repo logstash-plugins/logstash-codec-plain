ee85d2b9a91846cbb9a15f6829d84be95dbf6dd2
Suyog Rao <suyog.rao@gmail.com>
2014-11-07 07:42:10 -0800

    Added inline code asciidoc formatting

--- lib/logstash/codecs/plain.rb
+++ lib/logstash/codecs/plain.rb
@@ -10,19 +10,19 @@ class LogStash::Codecs::Plain < LogStash::Codecs::Base
   config_name "plain"
   milestone 3
 
-  # Set the message you which to emit for each event. This supports sprintf
+  # Set the message you which to emit for each event. This supports `sprintf`
   # strings.
   #
   # This setting only affects outputs (encoding of events).
   config :format, :validate => :string
 
-  # The character encoding used in this input. Examples include "UTF-8"
-  # and "cp1252"
+  # The character encoding used in this input. Examples include `UTF-8`
+  # and `cp1252`
   #
-  # This setting is useful if your log files are in Latin-1 (aka cp1252)
-  # or in another character set other than UTF-8.
+  # This setting is useful if your log files are in `Latin-1` (aka `cp1252`)
+  # or in another character set other than `UTF-8`.
   #
-  # This only affects "plain" format logs since json is UTF-8 already.
+  # This only affects "plain" format logs since json is `UTF-8` already.
   config :charset, :validate => ::Encoding.name_list, :default => "UTF-8"
 
   public
